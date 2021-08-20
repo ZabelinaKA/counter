c8f5a130f89cf05bc4918eaa88472b81c88ed801
Ksenia <ksu.zab2016@yandex.ru>
2021-08-20 23:16:52 +0500

    Counter

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun counter (view: View) {
+        val countString = EditText.text.tostring()
+        var count: Int = Integer.parseInt(countstring)
+        count++
+        EditText.text = count.Tostring()
+    }
+}
